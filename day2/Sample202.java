a6b671643b8443fbb6576f8899c4e119159cce6d
chiba <c.keigo1008@gmail.com>
2022-11-22 23:53:43 +0900

    Day2

---
+++ day2/Sample202.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class Sample202 {
+    public static void main(String[] args) {
+        int a;
+        int b = 3;
+        int add, sub;
+        double avg;
+        a = 6;
+        add = a + b;
+        sub = a - b;
+        avg = (a + b) / 2.0;
+        System.out.println("add : " + add);
+        System.out.println("sub : " + sub);
+        System.out.println("avg : " + avg);
+    }
+}
