be6fcc94e344beb2fe7d607611ac17745604479d
chiba <c.keigo1008@gmail.com>
2022-11-22 22:42:05 +0900

    Day1

--- day1/Sample101.java
+++ day1/Sample101.java
@@ -1,9 +1,6 @@
 package day1;
 
 public class Sample101 {
-    /*
-     * Javaを使った最も基本的なプログラム
-     */
     public static void main(String[] args) {
         System.out.println("Hello, World.");
     }
