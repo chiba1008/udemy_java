7b980d06e99b2c307bd394f31c8bc77c6b701a2a
chiba <c.keigo1008@gmail.com>
2022-11-24 15:44:43 +0900

    day3

---
+++ day3/Sample303.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class Sample303 {
+    public static void main(String[] args) {
+        int a = 0;
+        System.out.println("a= " + a);
+        if (a > 0) {
+            System.out.println("aは正の数です。");
+        } else if (a == 0) {
+            System.out.println("aは0です。");
+        } else {
+            System.out.println("aは正の数ではありません。");
+        }
+    }
+}
