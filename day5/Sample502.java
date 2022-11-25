1637233104138e0a34afdd7d1d6a233a810779d5
chiba <c.keigo1008@gmail.com>
2022-11-25 13:07:32 +0900

    day5

---
+++ day5/Sample502.java
@@ -0,0 +1,16 @@
+package day5;
+
+public class Sample502 {
+    public static void main(String[] args) {
+        int n[] = { 5, 4, 3, 2, 1 };
+        String s[] = { "ABC", "DEF", "GHI" };
+        int i;
+        for (i = 0; i < n.length; i++) {
+            System.out.println(n[i]);
+        }
+        for (i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
+    }
+
+}
