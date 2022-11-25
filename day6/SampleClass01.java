20e952b1752b34d181248e9979f0917be0f53260
chiba <c.keigo1008@gmail.com>
2022-11-25 13:48:03 +0900

    day6

---
+++ day6/SampleClass01.java
@@ -0,0 +1,20 @@
+package day6;
+
+public class SampleClass01 {
+    int n = 10;
+    String s = "field";
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    String add(String s) {
+        return this.s + s;
+    }
+
+    void showNum() {
+        System.out.println("n = " + n);
+        System.out.println("s = " + s);
+
+    }
+}
