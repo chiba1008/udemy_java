c497c3deb07d879729431cbf5e75d5986d7e05f0
chiba <c.keigo1008@gmail.com>
2022-11-28 12:31:20 +0900

    day7

---
+++ day7/Sample701.java
@@ -0,0 +1,12 @@
+package day7;
+
+public class Sample701 {
+
+    public static void main(String[] args) {
+        SampleClass02 s = new SampleClass02();
+
+        s.method2();
+        s.method3();
+
+    }
+}
